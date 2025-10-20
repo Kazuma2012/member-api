109787974ce94ffec24309522b5d300a7bdb1799
Kazuma2012 <akkazu1121@gmail.com>
2025-10-20 11:47:13 +0900

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,3 @@
-<!-- routes/userRoutes.js -->
-<script type="module">
 import express from 'express'
 import {
   registerUser,
@@ -15,10 +13,8 @@ const router = express.Router()
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 
-// 認証が必要なルートは verifyToken ミドルウェアを通す
 router.get('/users/:id', verifyToken, getUser)
 router.put('/users/:id', verifyToken, updateUser)
 router.delete('/users/:id', verifyToken, deleteUser)
 
 export default router
-</script>
